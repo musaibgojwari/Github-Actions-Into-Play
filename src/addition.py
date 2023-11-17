caee8a023a9d8e4c23513911db1d6bfa954859fa
Musaib Bashir <71263678+musaibgojwari@users.noreply.github.com>
2023-11-17 14:58:07 +0530

    Added subbraction.py

--- src/addition.py
+++ src/addition.py
@@ -2,7 +2,13 @@
 # This is a test commit
 def add(a, b):
     return a + b
+def sub(a,b):
+    return a-b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+def test_sub():
+    assert sub(12,0) == 12
+    assert sub(-76,-1) == -77
